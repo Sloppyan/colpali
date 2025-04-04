81743b7047304a8f0c0654077c637bcba48eabc1
NormalLLer <wanqingsi1438438@gmail.com>
2025-04-04 21:58:57 +0200

    chore: llava scripts

--- scripts/configs/llava_interleave/llava-scripts.sh
+++ scripts/configs/llava_interleave/llava-scripts.sh
@@ -3,3 +3,7 @@ pip install -e .[train]
 
 # Training
 USE_LOCAL_DATASET=0 python scripts/train/train_colbert.py scripts/configs/llava_interleave/train_colllavai_model.yaml
+
+## Training in Windows PowerShell
+# $env:USE_LOCAL_DATASET=0
+# python scripts/train/train_colbert.py scripts/configs/llava_interleave/train_colllavai_model.yaml
